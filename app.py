97712f8ef968ee957be24e37aca7077c3b1fdf91
Charlie Lyons <charlietlyons@gmail.com>
2022-01-21 16:11:09 -0600

    Load rates JSON from file

--- app.py
+++ app.py
@@ -3,38 +3,7 @@ import json, datetime
 
 app = Flask(__name__)
 
-rates = [
-    {
-        "days": "mon,tues,thurs",
-        "times": "0900-2100",
-        "tz": "America/Chicago",
-        "price": 1500
-    },
-    {
-        "days": "fri,sat,sun",
-        "times": "0900-2100",
-        "tz": "America/Chicago",
-        "price": 2000
-    },
-    {
-        "days": "wed",
-        "times": "0600-1800",
-        "tz": "America/Chicago",
-        "price": 1750
-    },
-    {
-        "days": "mon,wed,sat",
-        "times": "0100-0500",
-        "tz": "America/Chicago",
-        "price": 1000
-    },
-    {
-        "days": "sun,tues",
-        "times": "0100-0700",
-        "tz": "America/Chicago",
-        "price": 925
-    }
-]
+rates = json.load(open('rates.json'))['rates']
 
 def isActiveWeekday(weekday, rate):
     daysOfTheWeek = {

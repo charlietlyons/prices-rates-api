9bb46780b41fd70d8a087b025a7d0b36e7687f2d
Charlie Lyons <charlietlyons@gmail.com>
2022-01-21 15:58:16 -0600

    Price Get takes query params, returns unavailable if spanning more than one day + try except

--- app.py
+++ app.py
@@ -36,23 +36,58 @@ rates = [
     }
 ]
 
+def isActiveWeekday(weekday, rate):
+    daysOfTheWeek = {
+        'mon': 0,
+        'tues': 1,
+        'wed': 2,
+        'thurs': 3,
+        'fri': 4,
+        'sat': 5,
+        'sun': 6, 
+    }
+
+    for day in rate['days'].split(','):
+        if daysOfTheWeek[day] == weekday:
+            return True
+    return False
+
 @app.route("/rates", methods=['GET', 'PUT'])
 def getRates():
-    if request.method == 'GET':
-        return jsonify(rates)
-    if request.method == 'PUT':
-        data = json.loads(request.data)
-        timeFrame = data['times'].split('-')
+    try:
+        if request.method == 'GET':
+            return jsonify(rates)
+        elif request.method == 'PUT':
+            data = json.loads(request.data)
+            timeFrame = data['times'].split('-')
 
-        if timeFrame[1] < timeFrame[0]:
-            return Response('Cannot add rates. Time ranges must exist within the same day.', 503)
-        else:
-            rates.append(data)
-            return Response('Rate successfully added.', 200)
+            if timeFrame[1] < timeFrame[0]:
+                return Response('unavailable', 503)
+            else:
+                rates.append(data)
+                return Response('Rate successfully added.', 200)
+    except:
+        return Response('unavailable', 503)
 
-@app.route("/price")
+@app.route("/price", methods=['GET'])
 def getPrices():
-    return "some prices"
+    try:
+        requestStart = datetime.datetime.fromisoformat(request.args['start'].replace(' ', '+'))
+        requestEnd = datetime.datetime.fromisoformat(request.args['end'].replace(' ', '+'))
+
+        if requestStart > requestEnd or not requestStart.date() == requestEnd.date():
+            return Response('unavailable', 503)
+        else:
+            for rate in rates:
+                rateStart = datetime.datetime.strptime(rate['times'].split('-')[0], '%H%M')            
+                rateEnd = datetime.datetime.strptime(rate['times'].split('-')[1], '%H%M')
+                isWithinTimeFrame = requestStart.time() >= rateStart.time() and requestEnd.time() <= rateEnd.time()
+
+                if isWithinTimeFrame and isActiveWeekday(requestStart.weekday(), rate):
+                    return jsonify({'price': rate['price']})
+            raise Exception('No available rates')
+    except:
+        return Response('unavailable', 503)
 
 
 if __name__ == "__main__":
